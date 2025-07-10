a826f92bea3b075a9a2a97297180c7843e0d5fb7
LukasReckwald <lukas.reckwald@gmail.com>
2025-07-10 23:09:28 +0200

    add config

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,60 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Platform } from 'react-native';
+
+import { HapticTab } from '@/components/HapticTab';
+import { IconSymbol } from '@/components/ui/IconSymbol';
+import TabBarBackground from '@/components/ui/TabBarBackground';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+    const colorScheme = useColorScheme();
+
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+                headerShown: false,
+                tabBarButton: HapticTab,
+                tabBarBackground: TabBarBackground,
+                tabBarStyle: {
+                    backgroundColor: 'transparent',
+                    position: 'absolute',
+                    borderTopWidth: 0,
+                    elevation: 0,
+                },
+            }}
+        >
+            <Tabs.Screen
+                name="playlist"
+                options={{
+                    title: 'Playlist',
+                    tabBarIcon: ({ color }) => (
+                        <IconSymbol size={28} name="music.note.list" color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="mapview"
+                options={{
+                    title: 'Karte',
+                    tabBarIcon: ({ color }) => (
+                        <IconSymbol size={28} name="map.fill" color={color} />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Profil',
+                    tabBarIcon: ({ color }) => (
+                        <IconSymbol size={28} name="person.fill" color={color} />
+                    ),
+                }}
+            />
+        </Tabs>
+
+    );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,31 @@
-import { Stack } from "expo-router";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
-  return (
-      <Stack
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: '#f4511e',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-          }}>
-        {/* Optionally configure static options outside the route.*/}
-        <Stack.Screen name="index" options={{
-            title: 'Home',
-            headerShown: false,
-        }} />
-      </Stack>
-  );
+    const colorScheme = useColorScheme();
+    const [loaded] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    });
+
+    if (!loaded) {
+        // Async font loading only occurs in development.
+        return null;
+    }
+
+    return (
+        <ThemeProvider value={ DefaultTheme}>
+            <Stack>
+                <Stack.Screen name="index" options={{ headerShown: false }} />
+                <Stack.Screen name="login" options={{ headerShown: false }} />
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="+not-found" />
+            </Stack>
+            <StatusBar style="auto" />
+        </ThemeProvider>
+    );
 }
